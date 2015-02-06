dad1e937a8763bbd75efc5d7c6aab4a5ba0b9889
Peter Andersson <peter@snowfall.se>
2015-02-06 13:21:58 +0100

    tables

---
+++ tables.sql
@@ -0,0 +1,21 @@
+
+CREATE DATABASE wa CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+create table wash_users (
+id int not null auto_increment,
+name varchar(255),
+token char(32),
+primary key(id),
+unique(token)
+)engine=InnoDB;
+
+
+create table wash_urls(
+id int not null auto_increment,
+user_id int,
+url text,
+clicks int default 0,
+created datetime,
+primary key(id),
+foreign key uid(user_id) references wash_users(id) on delete cascade
+)engine=InnoDB;

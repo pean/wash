1c9210380dded5b6b57302503f49cbe15ab4420f
Peter Andersson <peter@snowfall.se>
2015-02-07 14:47:27 +0100

    Removed fauly empty check

--- src/Pean/Wash.php
+++ src/Pean/Wash.php
@@ -76,7 +76,7 @@ class Wash {
     $hash = end(explode('/',$url));
 
     // Nothng there. 
-    if(empty(preg_match('/^[a-zA-Z0-9]+$/',$hash))) {
+    if(!preg_match('/^[a-zA-Z0-9]+$/',$hash))) {
       return FALSE;
     }
 

61fe7c9b073e8d703356666a18007f7f356d2184
Peter Andersson <peter@snowfall.se>
2015-02-07 14:34:20 +0100

    moved things around some

--- src/Pean/Wash.php
+++ src/Pean/Wash.php
@@ -4,36 +4,31 @@ namespace Pean;
 
 class Wash {
 
-  public $hashids;
   protected $arr;
   protected $dbh;
   protected $config;
 
-  public function hello() {
-    echo "Hello Wash!";
-  }
-
-  function __construct($config) {
+  public function __construct($config) {
 
     $this->config = $config;
 
-    // TODO: Only do this if there is there is something to do 
-    $this->db(); 
-    $this->hashids();
+    if(empty($this->config['test'])) {
 
-    $input = file_get_contents("php://input");
-    $this->arr = json_decode($input, true);
+      $input = file_get_contents("php://input");
+      $this->arr = json_decode($input, true);
 
-    // Nothing submitted: Goto URL
-    if(empty($this->arr)) {
-      $this->gotoURL();
-    }
+      // Nothing submitted: Goto URL
+      $this->gotoURL($_SERVER['REQUEST_URI']);
 
-    // JSON submitted: Create url
-    elseif(!empty($this->arr['url'])) {
+      // JSON submitted: Create url
       $this->createURL();
+
     }
 
+  }
+
+  public function hello() {
+    return "Hello Wash!";
   }
 
   protected function ga($hash,$url_id,$url) {
@@ -67,54 +62,85 @@ class Wash {
     }
   } 
 
-  protected function hashids() {
-    // Init hasids
-    $this->hashids = new \Hashids\Hashids(md5($this->config['salt']));
+  protected function hashids($type,$str) {
+    $hashids = new \Hashids\Hashids(md5($this->config['salt']));
+    if($type == 'decode') {
+      return end($hashids->decode($str));
+    } elseif($type == "encode") {
+      return $hashids->encode($str);
+    }
   }
 
-  protected function gotoURL() {
-    $url = $_SERVER['REQUEST_URI'];
+  protected function identifyURL($url) {
+
     $hash = end(explode('/',$url));
 
     // Nothng there. 
-    if(empty($hash)) {
-      return false;
+    if(empty(preg_match('/^[a-zA-Z0-9]+$/',$hash))) {
+      return FALSE;
     }
 
-    $url_id = end($this->hashids->decode($hash));
-    if(empty($url_id)) {
-      $this->response(0,"errorMsg","Invalid url");
-    }
+    $url_id = $this->hashids('decode',$hash);
 
-    $sql = "select url from wash_urls where id=?";
-    $stmt = $this->dbh->prepare($sql);
-    try {
-      $stmt->execute(array($url_id));
-      $url = $stmt->fetchColumn();
-    } catch (PDOException $e) {
-      $this->response(0,"errorMsg","Could not fetch url: ".$e->getMessage());
+    if(preg_match('/^[\d]+$/',$url_id)) {
+      // Something there
+      return $url_id;
+    } else {
+      // Something not valid there
+      return FALSE;
     }
+  }
 
-    if(!empty($url)) {
-      $sql = "update wash_urls set clicks=clicks+1 where id=?";
+  protected function gotoURL($url, $output = 'redirect') {
+    if(empty($this->arr)) {
+
+      $url_id = $this->identifyURL($url);
+
+      if($url_id === FALSE) {
+        $this->response(0,"errorMsg","Invalid url");
+      }
+
+      $this->db(); 
+
+      $sql = "select url from wash_urls where id=?";
       $stmt = $this->dbh->prepare($sql);
       try {
         $stmt->execute(array($url_id));
+        $url = $stmt->fetchColumn();
       } catch (PDOException $e) {
-        $this->response(0,"errorMsg","Could count click: ".$e->getMessage());
+        $this->response(0,"errorMsg","Could not fetch url: ".$e->getMessage());
       }
 
-      // Track GA
-      $this->ga($hash,$url_id,$url);
+      if(!empty($url)) {
+        $sql = "update wash_urls set clicks=clicks+1 where id=?";
+        $stmt = $this->dbh->prepare($sql);
+        try {
+          $stmt->execute(array($url_id));
+        } catch (PDOException $e) {
+          $this->response(0,"errorMsg","Could count click: ".$e->getMessage());
+        }
 
-      // $this->response(1,"url",$url);
-      header('Location: '.$url);
-      exit;
+        // Track GA
+        $this->ga($hash,$url_id,$url);
+
+        if($output == 'JSON') {
+          $this->response(1,"url",$url);
+        } else {
+          header('Location: '.$url);
+          exit;
+        }
+      }
     }
-
   }
 
   protected function getUser() {
+
+    if(empty($this->arr['token'])) {
+      $this->response(0,"errorMsg","Missing token");
+    }
+
+    $this->db(); 
+
     $sql = "select id from wash_users where token=?";
     $stmt = $this->dbh->prepare($sql);
     try {
@@ -130,39 +156,39 @@ class Wash {
   }
 
   protected function createURL() {
-
-    // Get user
-    if(!empty($this->arr['token'])) {
+    if(!empty($this->arr['url'])) {
+     
+      // Get user
       $user_id = $this->getUser();
-    }
 
-    $sql = "select id from wash_urls where url=? && user_id=?";
-    $stmt = $this->dbh->prepare($sql);
-    try {
-      $stmt->execute(array($this->arr['url'],$user_id));
-      $url_id = $stmt->fetchColumn();
-    } catch (PDOException $e) {
-      $this->response(0,"errorMsg","Could not check if url existed: ".$e->getMessage());
-    }
-
-    if(empty($url_id)) {
-      $sql = "insert into wash_urls (user_id,url,created) values (?,?,now())";
+      $sql = "select id from wash_urls where url=? && user_id=?";
       $stmt = $this->dbh->prepare($sql);
       try {
-        $stmt->execute(array($user_id,$this->arr['url']));
+        $stmt->execute(array($this->arr['url'],$user_id));
+        $url_id = $stmt->fetchColumn();
       } catch (PDOException $e) {
-        $this->response(0,"errorMsg","Could not create link: ".$e->getMessage());
+        $this->response(0,"errorMsg","Could not check if url existed: ".$e->getMessage());
       }
-      // Get last insert id
-      $url_id = $this->dbh->lastInsertId();
-    }
-    // Generate the url
-    if(!empty($url_id)) {
-      $hash = $this->hashids->encode($url_id);
-      $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$hash;
-      // response(1,'url',$url);
-      echo $url;
-      exit;
+
+      if(empty($url_id)) {
+        $sql = "insert into wash_urls (user_id,url,created) values (?,?,now())";
+        $stmt = $this->dbh->prepare($sql);
+        try {
+          $stmt->execute(array($user_id,$this->arr['url']));
+        } catch (PDOException $e) {
+          $this->response(0,"errorMsg","Could not create link: ".$e->getMessage());
+        }
+        // Get last insert id
+        $url_id = $this->dbh->lastInsertId();
+      }
+      // Generate the url
+      if(!empty($url_id)) {
+        $hash = $this->hashids('encode',$url_id);
+        $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$hash;
+        // response(1,'url',$url);
+        echo $url;
+        exit;
+      }
     }
   }
 
